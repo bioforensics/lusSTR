7e871a62852ad459c11cfdcdad911784358beb27
Rebecca Eggebeen <rneggebeeen@Rebeccas-MacBook-Air.local>
2020-04-01 10:00:28 -0400

    style fixes

--- lusSTR/__main__.py
+++ lusSTR/__main__.py
@@ -7,7 +7,15 @@
 # and is licensed under the BSD license: see LICENSE.txt.
 # -----------------------------------------------------------------------------
 
-from lusSTR import annot
-from lusSTR.annot import str_dict
-from lusSTR import format
-from lusSTR import cli
+import argparse
+import lusSTR
+import sys
+
+
+def main(args=None):  # pragma: no cover
+    if args is None:  # pragma: no cover
+        if len(sys.argv) == 1:
+            lusSTR.cli.get_parser().parse_args(['-h'])
+        args = lusSTR.cli.get_parser().parse_args()
+    mainmethod = lusSTR.cli.mains[args.subcmd]
+    mainmethod(args)
