10e46890a51b93a6b16e465b6e23bcbcde1a74d8
Rebecca Mitchell <rebecca.mitchell@nbacc.dhs.gov>
2020-04-27 16:03:23 -0400

    added UAS annotate test

--- lusSTR/tests/test_suite.py
+++ lusSTR/tests/test_suite.py
@@ -12,7 +12,7 @@ import pandas as pd
 import pytest
 import lusSTR
 from lusSTR.tests import data_file
-from tempfile import NamedTemporaryFile
+from tempfile import NamedTemporaryFile, TemporaryDirectory
 
 
 def test_format():
@@ -258,3 +258,17 @@ def test_D21_lus_sec():
 def test_full_seq_to_uas(sequence, uas_seq, front, back):
     uas_sequence = lusSTR.annot.full_seq_to_uas(sequence, front, back)
     assert uas_sequence == uas_seq
+
+
+def test_annotate_uas():
+    with NamedTemporaryFile() as outfile:
+        input = data_file('2800M_formatted_uas.csv')
+        arglist = ['annotate', input, '-o', outfile.name, '--kit', 'forenseq', '--uas']
+        args = lusSTR.cli.get_parser().parse_args(arglist)
+        lusSTR.annot.main(args)
+        with open(outfile.name, 'r') as fh:
+            results = fh.read().strip('\n')
+        with open(data_file('2800M_uas_anno.txt'), 'r') as fh:
+            testresults = fh.read().strip('\n')
+        assert results == testresults
+
