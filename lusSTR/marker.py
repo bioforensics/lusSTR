564791e66732eba7a46015b14b9d99832cd59578
Daniel Standage <daniel.standage@gmail.com>
2020-05-27 23:44:05 -0400

    All locus-specific handling migrated

--- lusSTR/marker.py
+++ lusSTR/marker.py
@@ -9,7 +9,9 @@
 
 import json
 import lusSTR
+from lusSTR.annot import split_sequence_into_two_strings
 from lusSTR.repeat import collapse_repeats_by_length, sequence_to_bracketed_form
+from lusSTR.repeat import reverse_complement, reverse_complement_bracketed
 from pkg_resources import resource_filename
 import re
 
@@ -81,7 +83,7 @@ class STRMarker():
     @property
     def forward_sequence(self):
         if self.data['ReverseCompNeeded']:
-            return lusSTR.annot.reverse_complement(self.uas_sequence)
+            return reverse_complement(self.uas_sequence)
         return self.uas_sequence
 
     @property
@@ -144,17 +146,23 @@ class STRMarker():
         return not self.cannot_split or self.split_compatible
 
     @property
-    def collapsed(self):
-        if self.data['ReverseCompNeeded']:
+    def annotation(self):
+        if self.data['ReverseCompNeeded'] == 'Yes' and not self.cannot_split:
             collapseseq = collapse_repeats_by_length(self.forward_sequence, self.repeat_size)
         else:
             collapseseq = sequence_to_bracketed_form(
                 self.forward_sequence, self.repeat_size, self.repeats
             )
-        return collapsed
+        return collapseseq
 
     @property
-    def annotation(self):
+    def annotation_reverse(self):
+        if self.data['ReverseCompNeeded'] == 'Yes':
+            return reverse_complement_bracketed(self.collapsed)
+        return self.collapsed
+
+    @property
+    def designation(self):
         lus, sec, ter = None, None, None
         lus = repeat_copy_number(self.collapsed, self.data['LUS'])
         if self.data['Sec'] != '':
@@ -221,6 +229,42 @@ class STRMarker_D7S820(STRMarker):
         flank_seq = self.flankseq_3p
         return collapse_repeats_by_length(flank_seq, 4)
 
+    @property
+    def annotation(self):
+        '''
+        Function to correctly bracket microvariants in the D7S820 locus.
+
+        Microvariants N.1 and N.3 separate the first base (or first three bases) of the sequence
+        in order to correctly bracket the sequence. Microvariant N.2 has a third repeat motif
+        added in order to bracket the sequence correctly.
+        '''
+        if type(self.canonical) == int:
+            forward_strand_brack_form = sequence_to_bracketed_form(
+                self.forward_sequence, self.repeat_size, self.repeats
+            )
+        else:
+            if re.search(r'\d{1,2}.1', self.canonical):
+                if self.forward_sequence[-1] == 'T':
+                    forward_strand_brack_form = sequence_to_bracketed_form(
+                        self.forward_sequence, self.repeat_size, self.repeats
+                    )
+                else:
+                    bf = sequence_to_bracketed_form(
+                        self.forward_sequence[1:], self.repeat_size, self.repeats
+                    )
+                    forward_strand_brack_form = f'{self.forward_sequence[0]} {bf}'
+            elif re.search(r'\d{1,2}.2', self.canonical):
+                new_repeat_list = ['TATC', 'TGTC', 'AATC']
+                forward_strand_brack_form = sequence_to_bracketed_form(
+                    self.forward_sequence, self.repeat_size, new_repeat_list
+                )
+            else:
+                bf = sequence_to_bracketed_form(
+                    self.forward_sequence[3:], self.repeat_size, self.repeats
+                )
+                forward_strand_brack_form = f'{self.forward_sequence[:3]} {bf}'
+        return forward_strand_brack_form
+
 
 class STRMarker_D16S539(STRMarker):
     @property
@@ -251,6 +295,28 @@ class STRMarker_D1S1656(STRMarker):
     def flank_3p(self):
         return ''
 
+    @property
+    def annotation(self):
+        '''Bracketed annotation for D1S1656
+
+        This function identifies if the sequence is a microvariant in order to call different
+        functions to create the bracketed annotation.
+        '''
+        sequence_filt = self.forward_sequence[2:]
+        final = list()
+        first_string, second_string = split_sequence_into_two_strings(sequence_filt, 'CACA')
+        final.append(self.forward_sequence[:2])
+        if first_string == '':
+            final.append('CACA')
+        else:
+            final.append(collapse_repeats_by_length(first_string, 4))
+        if (len(second_string) % 4 != 0):
+            final.append(sequence_to_bracketed_form(second_string, 4, self.repeats))
+        else:
+            final.append(collapse_repeats_by_length(second_string, 4))
+        final_string = ' '.join(final)
+        return re.sub('  ', ' ', final_string)
+
 
 class STRMarker_PentaD(STRMarker):
     @property
@@ -260,6 +326,23 @@ class STRMarker_PentaD(STRMarker):
             f'{collapse_repeats_by_length(flank_seq[:20], 5)} {flank_seq[20]} {flank_seq[21:25]} '
             f'{collapse_repeats_by_length(flank_seq[25:], 5)}'
         )
+
+    @property
+    def annotation(self):
+        '''Bracketed annotation for PentaD
+
+        If the sequence is >= 18bp, the flanking region (first 5 bases) is first split off in the
+        sequence to preserve that sequence. Then the repeat units are identified and bracketed.
+        '''
+        if len(sequence) < 18:
+            return sequence_to_bracketed_form(self.uas_sequence, self.repeat_size, self.repeats)
+        else:
+            prefix = self.uas_sequence[:5]
+            suffix = self.uas_sequence[5:]
+            brack_form = sequence_to_bracketed_form(suffix, self.repeat_size, self.repeats)
+            result = f'{prefix} {brack_form}'
+            result = re.sub(r' +', ' ', result)
+            return result
 
 
 class STRMarker_vWA(STRMarker):
@@ -288,6 +371,78 @@ class STRMarker_FGA(STRMarker):
     def flank_3p(self):
         return ''
 
+    @property
+    def annotation(self):
+        '''Bracketed annotation for FGA
+
+        Specialized handling is required because which repeat unit should be identified differs
+        based on its location in the sequence. For example, the 'GGAA' repeat should be identified
+        at the beginning of the sequence; the 'GAAA' repeat should be identified at the end of the
+        sequence; and the repeat 'AAAG' should be identified within the two end repeats.
+
+        Simply identifying repeat units in a specified order does not result in the final
+        annotation which is consistent with previously published annotation for this locus.
+        '''
+        if len(self.forward_sequence) % self.repeat_size != 0:
+            return collapse_repeats_by_length(self.forward_sequence, self.repeat_size)
+        else:
+            sequence = self.forward_sequence
+            final = list()
+            prev = 0
+            if (len(sequence) % 4 == 0):
+                final_string = collapse_repeats_by_length(sequence, 4)
+            else:
+                for m in re.finditer('GGAA', sequence):
+                    if prev == 0 or m.start() == prev:
+                        prev = m.end()
+                    else:
+                        break
+                first_string = sequence[:prev]
+                second_string = sequence[prev:]
+                prev = 0
+                for m in re.finditer('AAAA', second_string):
+                    prev = m.start()
+                    break
+                if second_string[prev:(prev+6)] == 'AAAAAA':
+                    third_string = second_string[:prev+2]
+                    fourth_string = second_string[prev+2:]
+                elif prev == 0:
+                    third_string = second_string[:-6]
+                    fourth_string = second_string[-6:]
+                else:
+                    third_string = second_string[:prev]
+                    fourth_string = second_string[prev:]
+                final.append(collapse_repeats_by_length(first_string, 4))
+                final.append(sequence_to_bracketed_form(third_string, 4, self.repeats))
+                count = 0
+                tmp = list()
+                for element in re.split('GAAA', fourth_string):
+                    parts = element.split(',')
+                    for i in parts:
+                        if i == '':
+                            count += 1
+                        else:
+                            if count == 1:
+                                tmp.append('GAAA')
+                            elif count >= 2:
+                                tmp.append('[GAAA]' + str(count))
+                            count = 1
+                            if i == 'AAAAAA':
+                                tmp.append('AA AAAA')
+                            elif len(i) > 4:
+                                for x in split_by_n(i, 4):
+                                    tmp.append(x)
+                            else:
+                                tmp.append(i)
+                if parts[-1] == '' and count > 2:
+                    tmp.append('[GAAA]' + str(count-1))
+                elif parts[-1] == '' and count <= 2:
+                    tmp.append('GAAA')
+                last_string_final = ' '.join(tmp)
+                final.append(last_string_final)
+                final_string = ' '.join(final)
+                return re.sub(r' +', ' ', final_string)
+
 
 class STRMarker_CSF1PO(STRMarker):
     @property
@@ -307,12 +462,11 @@ class STRMarker_D18S51(STRMarker):
         )
 
     @property
-    def collapsed(self):
+    def annotation(self):
         if isinstance(self.canonical, str):
             return sequence_to_bracketed_form(self.uas_sequence, self.repeat_size, self.repeats)
         elif isinstance(self.canonical, int):
             return collapse_repeats_by_length(self.uas_sequence, self.repeat_size)
-
 
 
 class STRMarker_D21S11(STRMarker):
@@ -323,6 +477,108 @@ class STRMarker_D21S11(STRMarker):
             f'{flank_seq[:2]} {flank_seq[2]} {collapse_repeats_by_length(flank_seq[3:11], 4)} '
             f'{flank_seq[-1]}'
         )
+
+    @property
+    def annotation(self):
+        '''Bracketed annotation for D21
+
+        A specialized function is required for this locus due to the potential end of the sequence
+        containing 'TA TCTA' and other variants. This sequence needs to remain intact to conform
+        with the conventional annotation for this particular locus. However, if the 'TATCTA' is
+        included in a repeat unit, the repeat unit needs to be reported (i.e. [TCTA]2).
+        '''
+        forward_strand_brack_form = sequence_to_bracketed_form(
+            self.uas_sequence, self.data['NumBasesToSeparate'], self.repeats
+        )
+        prev = 0
+        for m in re.finditer(']', forward_strand_brack_form):
+            prev = m.end()
+        if (
+            prev == (len(forward_strand_brack_form) - 1) or
+            prev == (len(forward_strand_brack_form) - 2)
+           ):
+            return forward_strand_brack_form
+        else:
+            first_string = forward_strand_brack_form[:prev+2]
+            second_string = forward_strand_brack_form[prev+2:]
+            second_string_final = re.sub(' ', '', second_string)
+            if len(second_string_final) % 4 == 0:
+                split_second_string = collapse_repeats_by_length(second_string_final, 4)
+                final_string = f'{first_string} {second_string}'
+            elif len(second_string_final) == 6:
+                third_string = second_string_final[-6:-4]
+                fourth_string = second_string_final[-4:]
+                final_string = f'{first_string} {third_string} {fourth_string}'
+            elif len(second_string_final) % 4 == 2:
+                third_string = second_string_final[:-6]
+                fourth_string = second_string_final[-6:-4]
+                last_string = second_string_final[-4:]
+                third_string_final = collapse_repeats_by_length(third_string, 4)
+                final_string = f'{first_string} {third_string_final} {fourth_string} {last_string}'
+            else:
+                third_string = collapse_repeats_by_length(second_string_final, 4)
+                final_string = f'{first_string} {third_string}'
+            return re.sub('  ', ' ', final_string)
+
+
+class STRMarker_TH01(STRMarker):
+    @property
+    def annotation(self):
+        '''Bracketed annotation for TH01
+
+        Special handling is required for the microvariants of the TH01 locus because of the
+        insertion of the 'ATG' between the repeat units 'AATG'.
+        '''
+        strings = collapse_all_repeats(self.uas_sequence, self.repeats)
+        final_string = list()
+        for unit in strings.split(' '):
+            if '[' not in unit and len(unit) > 3 and (len(unit) % 4 != 0) and unit[:3] == 'ATG':
+                group1 = unit[:3]
+                final_string.append(group1)
+                for x in split_by_n(unit[3:], n=4):
+                    final_string.append(x)
+            else:
+                final_string.append(unit)
+        final_form = ' '.join(final_string)
+        return final_form
+
+
+class STRMarker_D19S433(STRMarker):
+    @property
+    def annotation(self):
+        '''Bracketed annotation for D19S433
+
+        A specialized function is required for this locus. The sequence is first broken into two
+        different strings. The two sets of sequences are processed separately in order to identify
+        the potential presence of a deletion in either sequence.
+
+        Simply identifying repeat units in a specified order does not result in the final
+        annotation which is consistent with previously published annotation for this locus.
+        '''
+        final = list()
+        last = 0
+        prev = 0
+        for m in re.finditer('CCTT', self.forward_sequence):
+            if m.start() == prev or m.start() == last:
+                prev = m.end()
+            else:
+                last = m.end()
+        final.append(sequence[:2])
+        first_string = sequence[2:prev]
+        second_string = sequence[prev:]
+        if (len(first_string) % 4 != 0):
+            final.append(sequence_to_bracketed_form(first_string, 4, self.repeats))
+        else:
+            final.append(collapse_repeats_by_length(first_string, 4))
+        if (len(second_string) % 4 != 0):
+            third_string = second_string[:-6]
+            final.append(collapse_repeats_by_length(third_string, 4))
+            final.append(second_string[-6:-4])
+            final.append(second_string[-4:])
+        else:
+            final.append(collapse_repeats_by_length(second_string, 4))
+        final_string = ' '.join(final)
+        return re.sub(r' +', ' ', final_string)
 
 
 def init_str_marker(locus, sequence, uas=True, kit='forenseq'):
@@ -342,6 +598,8 @@ def init_str_marker(locus, sequence, uas=True, kit='forenseq'):
         'CSF1PO': STRMarker_CSF1PO,
         'D18S51': STRMarker_D18S51,
         'D21S11': STRMarker_D21S11,
+        'TH01': STRMarker_TH01,
+        'D19S433': STRMarker_D19S433,
     }
     if locus in constructors:
         constructor = constructors[locus]
