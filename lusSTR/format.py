24e5e4e3ee68fa63fdba242fd3739632be3607af
Rebecca Mitchell <rebecca.mitchell@nbacc.dhs.gov>
2020-09-03 12:51:57 -0400

    fixed style error

--- lusSTR/format.py
+++ lusSTR/format.py
@@ -15,20 +15,40 @@ import re
 import sys
 
 
-def uas_load(input_file):
+def uas_load(input_file, sex=False):
     '''Format a UAS Sample Details Report (.xlsx) for use with `lusSTR annotate`.'''
     data = pd.read_excel(io=input_file, sheet_name=0)
+    sampleID = data.iloc[1, 1]
+    projectID = data.iloc[2, 1]
+    analysisID = data.iloc[3, 1]
     offset = data[data.iloc[:, 0] == "Coverage Information"].index.tolist()[0]
     results = data.iloc[offset + 2:]
     results.columns = data.iloc[offset + 1]
     results = results[results.Locus != 'Amelogenin'][['Locus', 'Reads', 'Repeat Sequence']]
-    results['SampleID'] = data.iloc[1, 1]
-    results['Project'] = data.iloc[2, 1]
-    results['Analysis'] = data.iloc[3, 1]
-    return results
+    results['SampleID'] = sampleID
+    results['Project'] = projectID
+    results['Analysis'] = analysisID
+    if sex is True:
+        y_data = pd.read_excel(io=input_file, sheet_name='Y STRs')
+        y_offset = y_data[y_data.iloc[:, 0] == "Coverage Information"].index.tolist()[0]
+        y_strs = y_data.iloc[y_offset + 2:]
+        y_strs.columns = y_data.iloc[y_offset + 1]
+        y_strs = y_strs[['Locus', 'Reads', 'Repeat Sequence']]
+        x_data = pd.read_excel(io=input_file, sheet_name='X STRs')
+        x_offset = x_data[x_data.iloc[:, 0] == "Coverage Information"].index.tolist()[0]
+        x_strs = x_data.iloc[x_offset + 2:]
+        x_strs.columns = x_data.iloc[x_offset + 1]
+        x_strs = x_strs[['Locus', 'Reads', 'Repeat Sequence']]
+        sex_final = pd.concat([y_strs, x_strs], ignore_index=True)
+        sex_final['SampleID'] = sampleID
+        sex_final['Project'] = projectID
+        sex_final['Analysis'] = analysisID
+    else:
+        sex_final = ''
+    return sex_final, results
 
 
-def strait_razor_concat(input_dir):
+def strait_razor_concat(input_dir, sex=False):
     '''Format a directory of STRait Razor output files for use with `lusSTR annotate`.'''
     locus_list = [
         'CSF1PO', 'D10S1248', 'D12S391', 'D13S317', 'D16S539', 'D17S1301', 'D18S51', 'D19S433',
@@ -60,9 +80,12 @@ def strait_razor_concat(input_dir):
 
 def main(args):
     if args.uas:
-        results = uas_load(args.input)
+        sex_results, results = uas_load(args.input, args.sex)
     else:
-        results = strait_razor_concat(args.input)
+        results = strait_razor_concat(args.input, args.sex)
     if args.out is None:
         args.out = sys.stdout
     results.to_csv(args.out, index=False)
+    if args.sex:
+        name = os.path.splitext(args.out)[0]
+        sex_results.to_csv(f'{name}_sexloci.csv', index=False)
