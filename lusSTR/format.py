1eaa0ca2dfc625c3908d22f83044aaa0cff7357e
Daniel Standage <daniel.standage@nbacc.dhs.gov>
2020-10-20 14:38:03 -0400

    Refactor the format module (#35)
    * Refactor "format" module, add test
    
    * Begin refactoring "lusstr format"
    
    * Full test coverage
    
    * Refactor STRaitRazor formatting code
    
    * OS directory separators
    
    Co-authored-by: Rebecca Mitchell <rebecca.mitchell@nbacc.dhs.gov>

--- lusSTR/format.py
+++ lusSTR/format.py
@@ -9,62 +9,59 @@
 
 import lusSTR
 import argparse
+import glob
 import os
 import pandas as pd
-import re
 import sys
 
 
-def uas_load(input_file, sex=False):
-    '''Format a UAS Sample Details Report (.xlsx) for use with `lusSTR annotate`.'''
-    if os.path.isdir(input_file):
-        myfiles = os.listdir(input_file)
-        autosomal_data = pd.DataFrame()
-        sex_data = pd.DataFrame()
-        for filename in sorted(myfiles):
-            filepath = os.path.join(input_file, filename)
-            sex_final, results = uas_format(filepath, sex)
-            autosomal_data = autosomal_data.append(results)
-            if sex is True:
-                sex_data = sex_data.append(sex_final)
+def uas_load(inpath, sexloci=False):
+    '''Format a UAS Sample Details Report (.xlsx) for use with `lusSTR annotate`.
+
+    The `inpath` argument can refer to a report file or a directory of report files. Any files
+    without the `.xlsx` file extension are ignored. The `sexloci` argument determines whether X and
+    Y chromosome STRs are included along with autosomal STRs.
+    '''
+    if os.path.isdir(inpath):
+        auto_strs = pd.DataFrame()
+        sex_strs = pd.DataFrame() if sexloci is True else None
+        files = glob.glob(os.path.join(inpath, '*.xlsx'))
+        for filename in sorted(files):
+            filepath = os.path.join(inpath, filename)
+            autodata, sexdata = uas_format(filepath, sexloci)
+            auto_strs = auto_strs.append(autodata)
+            if sexloci is True:
+                sex_strs = sex_strs.append(sexdata)
     else:
-        sex_data, autosomal_data = uas_format(input_file, sex)
-    return sex_data, autosomal_data
+        auto_strs, sex_strs = uas_format(inpath, sexloci)
+    return auto_strs, sex_strs
 
 
-def uas_format(input_file, sex=False):
-    data = pd.read_excel(io=input_file, sheet_name=0)
-    sampleID = data.iloc[1, 1]
-    projectID = data.iloc[2, 1]
-    analysisID = data.iloc[3, 1]
-    offset = data[data.iloc[:, 0] == "Coverage Information"].index.tolist()[0]
-    results = data.iloc[offset + 2:]
-    results.columns = data.iloc[offset + 1]
-    results = results[results.Locus != 'Amelogenin'][['Locus', 'Reads', 'Repeat Sequence']]
-    results['SampleID'] = sampleID
-    results['Project'] = projectID
-    results['Analysis'] = analysisID
-    if sex is True:
-        y_data = pd.read_excel(io=input_file, sheet_name='Y STRs')
-        y_offset = y_data[y_data.iloc[:, 0] == "Coverage Information"].index.tolist()[0]
-        y_strs = y_data.iloc[y_offset + 2:]
-        y_strs.columns = y_data.iloc[y_offset + 1]
-        y_strs = y_strs[['Locus', 'Reads', 'Repeat Sequence']]
-        x_data = pd.read_excel(io=input_file, sheet_name='X STRs')
-        x_offset = x_data[x_data.iloc[:, 0] == "Coverage Information"].index.tolist()[0]
-        x_strs = x_data.iloc[x_offset + 2:]
-        x_strs.columns = x_data.iloc[x_offset + 1]
-        x_strs = x_strs[['Locus', 'Reads', 'Repeat Sequence']]
-        sex_final = pd.concat([y_strs, x_strs], ignore_index=True)
-        sex_final['SampleID'] = sampleID
-        sex_final['Project'] = projectID
-        sex_final['Analysis'] = analysisID
-    else:
-        sex_final = ''
-    return sex_final, results
+def parse_str_table_from_sheet(infile, sheet, exclude=None):
+    table = pd.read_excel(io=infile, sheet_name=sheet)
+    offset = table[table.iloc[:, 0] == "Coverage Information"].index.tolist()[0]
+    data = table.iloc[offset + 2:]
+    data.columns = table.iloc[offset + 1]
+    if exclude is not None:
+        data = data[~data.Locus.isin(exclude)]
+    data = data[['Locus', 'Reads', 'Repeat Sequence']]
+    data['SampleID'] = table.iloc[1, 1]
+    data['Project'] = table.iloc[2, 1]
+    data['Analysis'] = table.iloc[3, 1]
+    return data
 
 
-def strait_razor_concat(input_dir, sex=False):
+def uas_format(infile, sexloci=False):
+    auto_strs = parse_str_table_from_sheet(infile, sheet=0, exclude=['Amelogenin'])
+    sex_strs = None
+    if sexloci is True:
+        y_strs = parse_str_table_from_sheet(infile, 'Y STRs')
+        x_strs = parse_str_table_from_sheet(infile, 'X STRs')
+        sex_strs = pd.concat([y_strs, x_strs], ignore_index=True)
+    return auto_strs, sex_strs
+
+
+def strait_razor_concat(indir, sexloci=False):
     '''Format a directory of STRait Razor output files for use with `lusSTR annotate`.'''
     locus_list = [
         'CSF1PO', 'D10S1248', 'D12S391', 'D13S317', 'D16S539', 'D17S1301', 'D18S51', 'D19S433',
@@ -79,40 +76,33 @@ def strait_razor_concat(input_dir, sex=False):
         'DYS458', 'DYS456', 'HPRTB', 'DXS8378', 'DXS7423', 'DXS7132', 'DXS10135', 'DXS10074',
         'DXS10103', 'DYS385'
     ]
-    myfiles = os.listdir(input_dir)
-    autosomal_data = pd.DataFrame()
-    xydata = pd.DataFrame()
-    for filename in sorted(myfiles):
-        name = re.sub('_STRaitRazor.txt', '', filename)
-        filepath = os.path.join(input_dir, filename)
-        data = pd.read_csv(
-            filepath, sep='\t', header=None,
+    auto_strs = pd.DataFrame()
+    sex_strs = pd.DataFrame() if sexloci is True else None
+    analysisID = os.path.basename(indir.rstrip(os.sep))
+    files = glob.glob(os.path.join(indir, '*_STRaitRazor.txt'))
+    for filename in sorted(files):
+        name = filename.replace('_STRaitRazor.txt', '').split(os.sep)[-1]
+        table = pd.read_csv(
+            filename, sep='\t', header=None,
             names=['Locus_allele', 'Length', 'Sequence', 'Forward_Reads', 'Reverse_Reads']
         )
-        data[['Locus', 'Allele']] = data.Locus_allele.str.split(":", expand=True)
-        data['Total_Reads'] = data['Forward_Reads'] + data['Reverse_Reads']
-        data['SampleID'] = name
-        data = data[['Locus', 'Total_Reads', 'Sequence', 'SampleID']]
-        auto_only_data = data[data.Locus.isin(locus_list)]
-        autosomal_data = autosomal_data.append(auto_only_data)
-        if sex is True:
-            sex_only_data = data[data.Locus.isin(sex_locus_list)]
-            xydata = xydata.append(sex_only_data)
-    analysisID = input_dir.rstrip(os.sep)
-    analysisID_final = os.path.basename(analysisID)
-    autosomal_data['Project'] = analysisID_final
-    autosomal_data['Analysis'] = analysisID_final
-    if sex is True:
-        xydata['Project'] = analysisID_final
-        xydata['Analysis'] = analysisID_final
-    return xydata, autosomal_data
+        table[['Locus', 'Allele']] = table.Locus_allele.str.split(":", expand=True)
+        table['Total_Reads'] = table['Forward_Reads'] + table['Reverse_Reads']
+        table['SampleID'] = name
+        table['Project'] = analysisID
+        table['Analysis'] = analysisID
+        table = table[['Locus', 'Total_Reads', 'Sequence', 'SampleID', 'Project', 'Analysis']]
+        auto_strs = auto_strs.append(table[table.Locus.isin(locus_list)])
+        if sexloci is True:
+            sex_strs = sex_strs.append(table[table.Locus.isin(sex_locus_list)])
+    return auto_strs, sex_strs
 
 
 def main(args):
     if args.uas:
-        sex_results, results = uas_load(args.input, args.sex)
+        results, sex_results = uas_load(args.input, args.sex)
     else:
-        sex_results, results = strait_razor_concat(args.input, args.sex)
+        results, sex_results = strait_razor_concat(args.input, args.sex)
     if args.out is None:
         args.out = sys.stdout
     results.to_csv(args.out, index=False)
--- lusSTR/tests/test_format.py
+++ lusSTR/tests/test_format.py
@@ -9,8 +9,9 @@
 
 import filecmp
 import lusSTR
-import os
 from lusSTR.tests import data_file
+import os
+from shutil import copytree
 from tempfile import NamedTemporaryFile
 
 
@@ -39,7 +40,7 @@ def test_format_stdout(capsys):
 
 def test_format_straitrazor():
     with NamedTemporaryFile() as outfile:
-        inputdb = data_file('STRait_Razor_test_output/')
+        inputdb = data_file('STRait_Razor_test_output')
         testformat = data_file('STRait_Razor_test_output.csv')
         arglist = ['format', inputdb, '-o', outfile.name]
         args = lusSTR.cli.get_parser().parse_args(arglist)
@@ -59,27 +60,40 @@ def test_format_sexloci_uas():
         assert filecmp.cmp(formatoutput, outfile_name_output) is True
 
 
-def test_format_sex_loci_straitrazor():
-    with NamedTemporaryFile() as outfile:
-        inputdb = data_file('STRait_Razor_test_output/')
-        testformat = data_file('testformat_sr_sexloci.csv')
-        arglist = ['format', inputdb, '-o', outfile.name, '--include-sex']
-        args = lusSTR.cli.get_parser().parse_args(arglist)
-        lusSTR.format.main(args)
-        outfile_name = os.path.splitext(outfile.name)[0]
-        outfile_name_output = f'{outfile_name}_sexloci.csv'
-        assert filecmp.cmp(testformat, outfile_name_output) is True
+def test_format_sex_loci_straitrazor(tmp_path):
+    inputdb = data_file('STRait_Razor_test_output')
+    exp_out = data_file('testformat_sr_sexloci.csv')
+    obs_out = str(tmp_path / 'sr.csv')
+    obs_out_sex = str(tmp_path / 'sr_sexloci.csv')
+    arglist = ['format', inputdb, '-o', obs_out, '--include-sex']
+    args = lusSTR.cli.get_parser().parse_args(arglist)
+    lusSTR.format.main(args)
+    assert filecmp.cmp(exp_out, obs_out_sex) is True
 
 
-def test_uas_directory():
-    with NamedTemporaryFile() as outfile:
-        inputdb = data_file('UAS_bulk_input/')
-        testformat_auto = data_file('UAS_bulk_test.csv')
-        testformat_sex = data_file('UAS_bulk_test_sexloci.csv')
-        arglist = ['format', inputdb, '-o', outfile.name, '--uas', '--include-sex']
-        args = lusSTR.cli.get_parser().parse_args(arglist)
-        lusSTR.format.main(args)
-        assert filecmp.cmp(testformat_auto, outfile.name) is True
-        outfile_name = os.path.splitext(outfile.name)[0]
-        outfile_name_output = f'{outfile_name}_sexloci.csv'
-        assert filecmp.cmp(testformat_sex, outfile_name_output) is True
+def test_uas_directory_autosomal_only(tmp_path):
+    inputdb = data_file('UAS_bulk_input')
+    copydb = str(tmp_path / 'UAS_bulk_input')
+    copytree(inputdb, copydb)
+    bogusfile = os.path.join(copydb, 'bogusfile.txt')
+    with open(bogusfile, 'w') as fh:
+        pass
+    exp_out_auto = data_file('UAS_bulk_test.csv')
+    obs_out_auto = str(tmp_path / 'format_output.csv')
+    arglist = ['format', '-o', obs_out_auto, '--uas', copydb]
+    args = lusSTR.cli.get_parser().parse_args(arglist)
+    lusSTR.format.main(args)
+    assert filecmp.cmp(exp_out_auto, obs_out_auto) is True
+
+
+def test_uas_directory_with_xy(tmp_path):
+    inputdb = data_file('UAS_bulk_input')
+    exp_out_auto = data_file('UAS_bulk_test.csv')
+    exp_out_sex = data_file('UAS_bulk_test_sexloci.csv')
+    obs_out_auto = str(tmp_path / 'format_output.csv')
+    obs_out_sex = str(tmp_path / 'format_output_sexloci.csv')
+    arglist = ['format', '-o', obs_out_auto, '--uas', '--include-sex', inputdb]
+    args = lusSTR.cli.get_parser().parse_args(arglist)
+    lusSTR.format.main(args)
+    assert filecmp.cmp(exp_out_auto, obs_out_auto) is True
+    assert filecmp.cmp(exp_out_sex, obs_out_sex) is True
