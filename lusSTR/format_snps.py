e5f6252fac77dfed77ef418a2c49cb072dc2aa9c
Rebecca Mitchell <rebecca.mitchell@nbacc.dhs.gov>
2021-01-15 15:53:51 -0500

    Handling multiple SNPs within one reported sequence in STRait Razor data; refactored code into different functions

--- lusSTR/format_snps.py
+++ lusSTR/format_snps.py
@@ -31,6 +31,23 @@ snp_type_dict = {
 }
 
 
+snps_within_loci = {
+    'mh16-MC1RB': {
+        'SNPs': ['rs1805005', 'rs1805006', 'rs2228479']
+    },
+    'mh16-MC1RC': {
+        'SNPs': [
+            'rs11547464',
+            'rs1805007',
+            'rs201326893_Y152OCH',
+            'rs1110400',
+            'rs1805008',
+            'rs885479'
+            ]
+    }
+}
+
+
 def uas_load(indir, type='i'):
     '''Format SNP data from UAS output files for use with `lusSTR annotate_snps`'''
     snp_final_output = pd.DataFrame()
@@ -83,6 +100,46 @@ def uas_format(infile, snp_type_arg):
     return snp_data
 
 
+def compile_row_of_snp_data(infile, snp, table_loc, type, name, analysis):
+    snp_df = []
+    if 'mh16' in snp:
+        locus_data = snps_within_loci[snp]
+        for k in range(0, len(locus_data['SNPs'])):
+            snp_id = locus_data['SNPs'][k]
+            row_tmp = collect_snp_info(infile, snp_id, table_loc, type, name, analysis)
+            snp_df.append(row_tmp)
+    else:
+        row_tmp = collect_snp_info(infile, snp, table_loc, type, name, analysis)
+        snp_df.append(row_tmp)
+    final_snp_df = pd.DataFrame(snp_df)
+    return final_snp_df
+
+
+def collect_snp_info(infile, snpid, j, type, name, analysis):
+    if snpid == 'N29insA':
+        snpid = 'rs312262906_N29insA'
+    metadata = snp_marker_data[snpid]
+    snp_type = metadata['Type']
+    seq = infile.iloc[j, 2]
+    expected_alleles = metadata['Alleles']
+    snp_loc = metadata['Coord']
+    snp_call = seq[snp_loc]
+    if snpid == 'rs312262906_N29insA' and snp_call == 'A':
+        snp_call = 'insA'
+    if snp_call in expected_alleles:
+        allele_flag = ''
+    else:
+        allele_flag = 'Allele call does not match expected allele!'
+    if type in snp_type or 'all' in type:
+        row_tmp = [
+            snpid, infile.iloc[j, 3], snp_call, snp_type_dict[snp_type], name,
+            analysis, analysis, allele_flag, seq, infile.iloc[j, 6]
+        ]
+    else:
+        row_tmp is None
+    return row_tmp
+
+
 def strait_razor_concat(indir, snp_type_arg):
     '''Format a directory of STRait Razor output files for use with `lusSTR annotate_snps`.'''
     snps_final = pd.DataFrame()
@@ -102,50 +159,33 @@ def strait_razor_concat(indir, snp_type_arg):
                 f' and rerun the command, if necessary.'
             )
             continue
-        snps = []
+        snps = pd.DataFrame()
         if 'all' in snp_type_arg:
-            print('yes')
             snps_only = pd.DataFrame(
-                table[table['SNP'].str.contains('rs')]
+                table[table['SNP'].str.contains('rs|mh16|insA')]
             ).reset_index(drop=True)
             for j, row in snps_only.iterrows():
                 snpid = snps_only.iloc[j, 5]
                 try:
-                    metadata = snp_marker_data[snpid]
+                    row = compile_row_of_snp_data(snps_only, snpid, j, 'all', name, analysisID)
                 except KeyError:
                     continue
-                snp_type = metadata['Type']
-                seq = snps_only.iloc[j, 2]
-                snp_call = seq[metadata['Coord']]
-                row_tmp = [
-                    snpid, snps_only.iloc[j, 3], snp_call, snp_type_dict[snp_type], name,
-                    analysisID, analysisID, seq, snps_only.iloc[j, 6]
-                ]
-                snps.append(row_tmp)
+                snps = snps.append(row)
         else:
             for type in snp_type_arg:
                 for j, row in table.iterrows():
                     snpid = table.iloc[j, 5]
                     try:
-                        metadata = snp_marker_data[snpid]
+                        row = compile_row_of_snp_data(table, snpid, j, type, name, analysisID)
                     except KeyError:
                         continue
-                    snp_type = metadata['Type']
-                    seq = snps_only.iloc[j, 3]
-                    snp_call = seq[metadata['Coord']]
-                    if type in snp_type:
-                        row_tmp = [
-                            snpid, snps_only.iloc[j, 3], snp_call, snp_type_dict[snp_type], name,
-                            analysisID, analysisID, seq, snps_only.iloc[j, 6]
-                        ]
-                        snps.append(row_tmp)
-        snps_tmp = pd.DataFrame(snps)
-        snps_final = snps_final.append(snps_tmp)
-        snps_final.columns = [
-            'SNP', 'Reads', 'Allele', 'Type', 'SampleID', 'Project', 'Analysis', 'Sequence',
-            'Bases_off'
+                    if row is not None:
+                        snps = snps.append(row)
+        snps.columns = [
+            'SNP', 'Reads', 'Allele', 'Type', 'SampleID', 'Project', 'Analysis',
+            'Potential_Issues', 'Sequence', 'Bases_off'
         ]
-    return snps_final
+    return snps
 
 
 def main(args):
