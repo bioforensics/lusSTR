79428860060ca1332d70bd1109cf46d2dc58bb19
Rebecca Mitchell <rebecca.mitchell@nbacc.dhs.gov>
2021-01-26 07:20:23 -0500

    Added tests to check # of lines for type specified output

--- lusSTR/tests/test_snps.py
+++ lusSTR/tests/test_snps.py
@@ -27,6 +27,20 @@ def test_uas_all(tmp_path):
     assert filecmp.cmp(exp_out, obs_out) is True
 
 
+@pytest.mark.parametrize('type, lines', [
+    ('i', 132),
+    ('p', 95)
+])
+def test_uas_type(type, lines, tmp_path):
+    inputdb = data_file('snps')
+    obs_out = str(tmp_path / 'uas.txt')
+    arglist = ['snps', inputdb, '-o', obs_out, '--type', type, '--uas']
+    args = lusSTR.cli.get_parser().parse_args(arglist)
+    lusSTR.snps.main(args)
+    with open(obs_out, 'r') as fh:
+        assert len(fh.readlines()) == lines
+
+
 def test_sr_all(tmp_path):
     inputdb = data_file('snps')
     exp_out = data_file('snps_sr_all.txt')
@@ -38,3 +52,20 @@ def test_sr_all(tmp_path):
     lusSTR.snps.main(args)
     assert filecmp.cmp(exp_out, obs_out) is True
     assert filecmp.cmp(exp_out_full, obs_out_full) is True
+
+
+@pytest.mark.parametrize('type, lines, full_lines', [
+    ('i', 181, 2152),
+    ('p', 158, 2982)
+])
+def test_sr_type(type, lines, full_lines, tmp_path):
+    inputdb = data_file('snps')
+    obs_out = str(tmp_path / 'sr.txt')
+    obs_out_full = str(tmp_path / 'sr_full_output.txt')
+    arglist = ['snps', inputdb, '-o', obs_out, '--type', type]
+    args = lusSTR.cli.get_parser().parse_args(arglist)
+    lusSTR.snps.main(args)
+    with open(obs_out, 'r') as fh:
+        assert len(fh.readlines()) == lines
+    with open(obs_out_full, 'r') as fh:
+        assert len(fh.readlines()) == full_lines
