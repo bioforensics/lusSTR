f507eaa19a7c9d1ed0d20f9f8a78d6dfb4eec4d9
rnmitchell <rebecca.mitchell5987@gmail.com>
2023-10-06 13:15:42 -0400

    fixed test

--- lusSTR/tests/test_filters.py
+++ lusSTR/tests/test_filters.py
@@ -378,7 +378,7 @@ def test_forward_strand_orientation(tmp_path):
 def test_lusplus_sequence_info(tmp_path):
     str_path = str(tmp_path / "WD")
     inputfile = data_file("test_stutter_lusp.txt")
-    exp_out = data_file("LUSPlus_stutter_test/LUSplus_sequence_info.csv")
+    exp_out = data_file("LUSPlus_stutter_test/LUSPlus_sequence_info.csv")
     obs_out = str(tmp_path / f"WD/LUSplus/LUSPlus_sequence_info.csv")
     arglist = [
         "config",
