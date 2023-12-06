60ab0e5ab8fd1ff0ce2412a6b26b7511d8db88db
rnmitchell <rebecca.mitchell5987@gmail.com>
2023-12-06 10:45:24 -0500

    fixed manifest and setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
             "lusSTR/tests/data/kinsnps/*",
             "lusSTR/tests/data/lusstr_output/*",
             "lusSTR/tests/data/LUSPlus_stutter_test/*",
-            "lusSTR/tests/data/MPSProto_test/*",
+            "lusSTR/tests/data/MPSproto_test/*",
             "lusSTR/workflows/*",
             "lusSTR/wrappers/*",
         ]
