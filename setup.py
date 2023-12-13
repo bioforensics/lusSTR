a82438c051ce91c87392d8452dbc91510e9bcfe4
rnmitchell <rebecca.mitchell5987@gmail.com>
2023-12-13 15:07:07 -0500

    added package to setup.py

--- setup.py
+++ setup.py
@@ -45,6 +45,7 @@ setup(
         "snakemake>=7.22.0",
         "pyyaml>=6.0",
         "matplotlib>=3.5.3",
+        "importlib_resources>=6.1.1",
     ],
     entry_points={"console_scripts": ["lusstr = lusSTR.cli:main"]},
     scripts=glob.glob("lusSTR/scripts/*"),
