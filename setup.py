11f69ed2131b32b7391ddc8a2ba10f60c08b8d63
rnmitchell <rebecca.mitchell5987@gmail.com>
2023-12-28 14:20:05 -0500

    update snakemake version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     install_requires=[
         "pandas>=1.0,<2.0",
         "openpyxl>=3.0.6",
-        "snakemake>=7.22.0",
+        "snakemake>=8.1.3",
         "pyyaml>=6.0",
         "matplotlib>=3.8",
     ],
