aa64bf1877db25daf0bb74ed751f0598b695e085
rnmitchell <rebecca.mitchell5987@gmail.com>
2023-12-29 11:23:02 -0500

    changed to stderr message

--- lusSTR/wrappers/convert.py
+++ lusSTR/wrappers/convert.py
@@ -25,6 +25,7 @@ from lusSTR.scripts.repeat import sequence_to_bracketed_form, split_by_n
 from lusSTR.scripts.repeat import reverse_complement, reverse_complement_bracketed
 from matplotlib.backends.backend_pdf import PdfPages
 from pathlib import Path
+import sys
 
 
 with open(get_str_metadata_file(), "r") as fh:
@@ -109,9 +110,9 @@ def format_table(input, uas=False, kit="forenseq"):
                 if check_sr > 10:
                     msg = (
                         "Multiple microvariants identified at D12 locus. "
-                        "Please check STRait Razor version!!"
+                        "Please check STRait Razor version!!\n"
                     )
-                    print(msg)
+                    sys.stderr.write(msg)
         summary = [sampleid, project, analysis, locus] + marker.summary + [reads]
         list_of_lists.append(summary)
 
