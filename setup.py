fba877af3b0ca35526b0a7365bf10c973218fe6c
rnmitchell <57150382+rnmitchell@users.noreply.github.com>
2024-07-24 09:03:44 -0400

    Fix bug in setup.py script (#77)

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
             "lusSTR/tests/data/snps/*",
             "lusSTR/tests/data/RU_stutter_test/*",
             "lusSTR/tests/data/NGS_stutter_test/*",
+            "lusSTR/tests/data/NGS_stutter_test/custom/*",
             "lusSTR/tests/data/kinsnps/*",
             "lusSTR/tests/data/lusstr_output/*",
             "lusSTR/tests/data/LUSPlus_stutter_test/*",
